60edae18441139ffcd1b7c10e20e8bdb3be30ea4
Adrian "Fox" Cieluch <adrian.cieluch@gmail.com>
2017-02-23 23:44:50 +0100

    Added some commentaries

--- init.py
+++ init.py
@@ -4,5 +4,9 @@ import os
 
 # Install Ansible
 os.system('sudo apt-get install ansible -y')
+
+# Copy the ansible cfg to grant access to our custom roles
 os.system('cp ./config/.ansible.cfg ~/.ansible.cfg')
+
+# Execute the setup playbook on the local machine
 os.system('ansible-playbook -i ./Ansible/Inventories/local.yml ./Ansible/Playbooks/full_setup.yml')
